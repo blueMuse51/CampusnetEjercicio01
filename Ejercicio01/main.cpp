423b7c441fe76a122c733a0ae457d62f13d5b533
Marina Tchamitchian <marina.tchami11@gmail.com>
2021-09-30 18:01:47 +0100

    Cambiar frase

--- Ejercicio01/main.cpp
+++ Ejercicio01/main.cpp
@@ -4,6 +4,6 @@ int main()
 {
 	std //es un especie de carpeta de codigo conceptual, lleva muchas librerillas de entro
 		::cout //out put
-		<< "Hello world!" <<
+		<< "Hello Mar!" <<
 		std::endl; //es un salto de linea como \n
 }
