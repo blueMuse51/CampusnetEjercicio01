893b97a3a3e55fc6238a26a6d35a3c0fe300b1f1
blueMuse51 <91690025+blueMuse51@users.noreply.github.com>
2021-09-30 18:12:15 +0100

    Cambio Remoto

--- Ejercicio01/main.cpp
+++ Ejercicio01/main.cpp
@@ -4,6 +4,6 @@ int main()
 {
 	std //es un especie de carpeta de codigo conceptual, lleva muchas librerillas de entro
 		::cout //out put
-		<< "Hello Mar!" <<
+		<< "Hello!" <<
 		std::endl; //es un salto de linea como \n
 }
