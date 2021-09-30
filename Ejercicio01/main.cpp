fce9d08d3017c5baf4a8397fad185aa88181255f
marin <marin@MSI>
2021-09-30 17:55:01 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio01/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //libreria
+
+int main()
+{
+	std //es un especie de carpeta de codigo conceptual, lleva muchas librerillas de entro
+		::cout //out put
+		<< "Hello world!" <<
+		std::endl; //es un salto de linea como \n
+}
